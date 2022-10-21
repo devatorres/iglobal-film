ce44d19392a346b72d60e2d72656834c8df92342
atorres-io <atorresalcala.me@gmail.com>
2022-10-21 11:33:42 +0200

    build(library): tests added

---
+++ src/tests/App.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom'
+import App from 'App'
+
+test('should move to up when button is clicked', () => {
+  render(<App />)
+
+  const button = screen.getByRole('button')
+  userEvent.click(button)
+
+  const scroll = window.scroll
+  expect(scroll).toHaveBeenCalledWith(0, 0)
+})
